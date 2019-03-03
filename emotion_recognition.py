3dbfc2f4b02b57c038939a414e8693485de6a834
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-04 00:02:39 +0530

    up

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -5,8 +5,12 @@ Created on Sun Mar  3 22:46:17 2019
 
 @author: abhijithneilabraham
 """
+#step 1 goes here
 import cv2
 import dlib
 cap=cv2.VideoCapture(0)
+det=dlib.get_frontal_face_detector()
+pred=dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 while(1):
-    cap
+    ret,frame=cap.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)

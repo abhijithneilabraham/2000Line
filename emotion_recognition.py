be64f78843cbb62ca5944b25c2a39255e380a86d
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-03 23:48:59 +0530

    update

---
+++ emotion_recognition.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 22:46:17 2019
+
+@author: abhijithneilabraham
+"""
+import cv2
+import dlib
+cap=cv2.VideoCapture(0)
+while(1):
+    cap

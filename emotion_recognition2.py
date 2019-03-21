e9340141796542970d534f6ff374f9712c22efc7
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-21 22:41:00 +0530

    face recognition

--- emotion_recognition2.py
+++ emotion_recognition2.py
@@ -6,3 +6,34 @@ Created on Wed Mar 13 01:42:26 2019
 @author: abhijithneilabraham
 """
 
+import cv2
+import dlib
+import numpy as np
+import math
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+def get_landmarks(image):
+    detections = detector(image, 1)
+    for k,d in enumerate(detections): #For all detected face instances individually
+        shape = predictor(image, d) #Draw Facial Landmarks with the predictor class
+        xlist = []
+        ylist = []
+        for i in range(1,68): #Store X and Y coordinates in two lists
+            xlist.append(float(shape.part(i).x))
+            ylist.append(float(shape.part(i).y))
+        xmean = np.mean(xlist) #Find both coordinates of centre of gravity
+        ymean = np.mean(ylist)
+        xcentral = [(x-xmean) for x in xlist] #Calculate distance centre <-> other points in both axes
+        ycentral = [(y-ymean) for y in ylist]
+        landmarks_vectorised = []
+        for x, y, w, z in zip(xcentral, ycentral, xlist, ylist):
+            landmarks_vectorised.append(w)
+            landmarks_vectorised.append(z)
+            meannp = np.asarray((ymean,xmean))
+            coornp = np.asarray((z,w))
+            dist = np.linalg.norm(coornp-meannp)
+            landmarks_vectorised.append(dist)
+            landmarks_vectorised.append((math.atan2(y, x)*360)/(2*math.pi))
+        data['landmarks_vectorised'] = landmarks_vectorised
+    if len(detections) < 1:
+        data['landmarks_vestorised'] = "error"

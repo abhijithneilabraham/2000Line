07f8a4cafde7224b045e81f55263054595cfa204
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-23 11:19:54 +0530

    Update emotiontest2.py

--- emotiontest2.py
+++ emotiontest2.py
@@ -6,6 +6,11 @@ Created on Sat Mar 23 10:56:52 2019
 @author: abhijithneilabraham
 """
 import numpy as np
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Flatten,Dropout, AveragePooling2D
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+num_classes=7
 with open("/Users/abhijithneilabraham/Documents/challenges-in-representation-learning-facial-expression-recognition-challenge/fer2013/fer2013.csv") as f:
     content = f.readlines()
      
@@ -13,3 +18,50 @@ with open("/Users/abhijithneilabraham/Documents/challenges-in-representation-lea
      
     num_of_instances = lines.size
     print("number of instances: ",num_of_instances)
+x_train, y_train, x_test, y_test = [], [], [], []
+ 
+for i in range(1,num_of_instances):
+ try:
+  emotion, img, usage = lines[i].split(",")
+ 
+  val = img.split(" ")
+  pixels = np.array(val, 'float32')
+ 
+  emotion = keras.utils.to_categorical(emotion, num_classes)
+ 
+  if 'Training' in usage:
+   y_train.append(emotion)
+   x_train.append(pixels)
+  elif 'PublicTest' in usage:
+   y_test.append(emotion)
+   x_test.append(pixels)
+ except:
+  print("", end="")
+model=Sequential()
+model = Sequential()
+ 
+#1st convolution layer
+model.add(Conv2D(64, (5, 5), activation='relu', input_shape=(48,48,1)))
+model.add(MaxPooling2D(pool_size=(5,5), strides=(2, 2)))
+ 
+#2nd convolution layer
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(AveragePooling2D(pool_size=(3,3), strides=(2, 2)))
+ 
+#3rd convolution layer
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(AveragePooling2D(pool_size=(3,3), strides=(2, 2)))
+ 
+model.add(Flatten())
+ 
+#fully connected neural networks
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.2))
+ 
+model.add(Dense(num_classes, activation='softmax'))
+
+

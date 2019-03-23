d3222ead458629e9c940e119903e4d1639a9dbe1
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-23 11:37:40 +0530

    Update emotiontest2.py

--- emotiontest2.py
+++ emotiontest2.py
@@ -10,7 +10,8 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Flatten,Dropout, AveragePooling2D
 from keras.layers.convolutional import Conv2D, MaxPooling2D
-num_classes=7
+from keras.preprocessing.image import ImageDataGenerator
+num_classes=None
 with open("/Users/abhijithneilabraham/Documents/challenges-in-representation-learning-facial-expression-recognition-challenge/fer2013/fer2013.csv") as f:
     content = f.readlines()
      
@@ -37,8 +38,10 @@ for i in range(1,num_of_instances):
    x_test.append(pixels)
  except:
   print("", end="")
+print(len(x_train),len(y_train))
+
 model=Sequential()
-model = Sequential()
+
  
 #1st convolution layer
 model.add(Conv2D(64, (5, 5), activation='relu', input_shape=(48,48,1)))
@@ -62,6 +65,28 @@ model.add(Dropout(0.2))
 model.add(Dense(1024, activation='relu'))
 model.add(Dropout(0.2))
  
-model.add(Dense(num_classes, activation='softmax'))
+model.add(Dense(7, activation='softmax'))
+gen = ImageDataGenerator()
+batch_size=32
+epochs=20
+
+train_generator = gen.flow(x_train, y_train, batch_size=batch_size)
+ 
+model.compile(loss='categorical_crossentropy'
+, optimizer=keras.optimizers.Adam()
+, metrics=['accuracy']
+)
+ 
+model.fit_generator(train_generator, steps_per_epoch=batch_size, epochs=epochs)
+train_score = model.evaluate(x_train, y_train, verbose=0)
+print('Train loss:', train_score[0])
+print('Train accuracy:', 100*train_score[1])
+ 
+test_score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', test_score[0])
+print('Test accuracy:', 100*test_score[1])
+
+
+
 
 
